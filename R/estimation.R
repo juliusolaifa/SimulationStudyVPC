c94265c2c3a054fabbee0bae3358dca1c44e767c
juliusolaifa <jbolaifa@gmail.com>
2024-09-20 18:46:47 -0500

    package improvement

--- R/estimation.R
+++ R/estimation.R
@@ -2,12 +2,16 @@
 #'
 #' @param params A matrix or data frame of parameter values for GLMM data
 #' generation.
-#' @param covariates A vector or matrix of covariates for the GLMM.
-#' @param X A design matrix for the GLMM.
 #' @param ns Number of samples or observations per batch.
-#' @param true_family The family distribution assumed to be correct.
-#' @param false_family An alternative family distribution to compare.
-#' @param formula The model formula for GLMM fitting.
+#' @param gen_family The family distribution used for data generation.
+#' @param another_family An alternative family distribution to compare.
+#' @param gen_link A link function for generating the data
+#' @param fit_formula The model formula for GLMM fitting.
+#' @param X A design matrix for the GLMM.
+#' @param vpc_input_values A vector for which vpc is calculated
+#' @param num_cores Number of cores to fit GLMM with
+#' @param fit_transform logical wether a VST model should be fitter and its vpc
+#' computed.
 #' @param iter Number of iterations for data generation (default is 1).
 #' @param seed An optional seed for reproducibility.
 #'
@@ -15,50 +19,85 @@
 #' and VST-transformed model (if applicable).
 #'
 #' @export
-compareGLMMFit <- function(params, covariates, X, ns, true_family,
-                           false_family, formula, iter = 1, seed = NULL) {
+generatecompareEstimation <- function(params, ns, gen_family,
+                              another_family, gen_link, fit_formula,
+                              X = NULL, vpc_input_values = NULL,num_cores=1,
+                              fit_transform=TRUE,iter = 1, seed = NULL){
   if (!is.null(seed)) {
     set.seed(seed)
   }
 
-  print(paste("Generating Counts from ", true_family))
-  datafrmMat <- vpc::parallelbatchGLMMData(params, X, ns, family = true_family,
-                                      iter = iter)
+  vpc_true <- as.data.frame(sapply(vpc_input_values, function(x) {
+    result <- glmmVpc::vpc_from_paramMat(family=gen_family, paramMat=params, x = x)
+    as.numeric(result)
+  }))
 
-  ys <- datafrmMat[-1, ]
-  X <- datafrmMat[1, ]
-  group <- colnames(datafrmMat)
+  colnames(vpc_true) <- paste0("vpc", vpc_input_values)
 
-  print("Tranforming Counts with VST")
-  vst_ys <- tryCatch({
-    vpc::vstransform(ys, num_cores=4)
-  }, error = function(e) {
-    message("Error in VST transformation: ", e)
-    return(NULL)
-  })
+  datafrmMat <- glmmVpc::batchGLMMDataUsingMatrix(paramMat=params,
+                                                   ns=ns, X=X,
+                                                   family = gen_family,
+                                                   link=gen_link, iter = iter,
+                                                   parallel = TRUE)
 
-  print(paste("Fitting GLMM to", true_family, "Data Generated from", true_family))
-  fit_true <- vpc::batchGLMMFit(formula, ys, X, group, family = true_family,
-                           cov_values = covariates)
+  result <- compareEstimation(dataMat=datafrmMat, fit_formula=fit_formula,
+                                family1=gen_family, family2=another_family,
+                                fit_transform=fit_transform,
+                                vpc_input_values = vpc_input_values,
+                                num_cores=num_cores)
 
-  print(paste("Fitting GLMM to", false_family, "Data Generated from", true_family))
-  fit_false <- vpc::batchGLMMFit(formula, ys, X, group, family = false_family,
-                            cov_values = covariates)
-
-  if (!is.null(vst_ys)) {
-    print("Fitting GLMM to VST transformed data")
-    fit_vst <- vpc::batchGLMMFit(formula, vst_ys, X, group, family = "ga",
-                                 cov_values = covariates)
-    print("Obtaining coef for VST and its Vpcs")
-    coefs_vst <- stats::coef(fit_vst)
-  } else {
-    coefs_vst <- NULL
-  }
-
-  print(paste("Obtaining coef for", true_family, "and it's vpcs"))
-  coefs_true <- stats::coef(fit_true)
-  print(paste("Obtaining coef for", false_family, "and it's vpcs"))
-  coefs_false <- stats::coef(fit_false)
-
-  return(list(coefs_true = coefs_true, coefs_false = coefs_false, coefs_vst = coefs_vst))
+  result$true <- vpc_true
+  return(result)
 }
+
+
+compareEstimation <- function(dataMat, fit_formula,
+                              family1, family2,
+                              fit_transform=TRUE,
+                              vpc_input_values,
+                              num_cores=1) {
+
+  fit_family1 <- glmmVpc::batchGLMMFit(formula=fit_formula,
+                                    dataMat=dataMat,
+                                    family = family1,
+                                    num_cores=num_cores)
+  vpc_family1 <- as.data.frame(sapply(vpc_input_values, function(x) {
+    result <- glmmVpc::vpc(model_fit = fit_family1, x = x)
+    as.numeric(result)
+  }))
+
+  fit_family2 <- glmmVpc::batchGLMMFit(formula=fit_formula,
+                                    dataMat=dataMat,
+                                    family = family2,
+                                    num_cores=num_cores)
+  vpc_family2 <- as.data.frame(sapply(vpc_input_values, function(x) {
+    result <- glmmVpc::vpc(model_fit = fit_family2, x = x)
+    as.numeric(result)
+  }))
+  col_names <- paste0("vpc", vpc_input_values)
+  colnames(vpc_family1) <- colnames(vpc_family2)  <- col_names
+  result <- list()
+  result[[family1]] <- vpc_family1
+  result[[family2]] <- vpc_family2
+
+  if(fit_transform) {
+    ys <- glmmVpc::get_y(dataMat)
+    X <- glmmVpc::get_x(dataMat)
+    cluster <- glmmVpc::get_cluster(dataMat)
+    vst_ys <- transform_counts(ys, glmmVpc::vstransform, num_cores)
+    dataMat <- glmmVpc::makeDataMatrix(X,vst_ys,cluster)
+    fit_vst <- glmmVpc::batchGLMMFit(formula=fit_formula,
+                                       dataMat=dataMat,
+                                       family = "gaussian",
+                                       num_cores=num_cores)
+    vpc_vst <- as.data.frame(sapply(vpc_input_values, function(x) {
+      result <- glmmVpc::vpc(model_fit = fit_vst, x = x)
+      as.numeric(result)
+    }))
+    colnames(vpc_vst) <- col_names
+    result$gaussian <- vpc_vst
+  }
+  class(result) <- "vpcest"
+  return(result)
+}
+
--- R/utils.R
+++ R/utils.R
@@ -161,4 +161,72 @@ robust_cbind <- function(df1, df2) {
 }
 
 
+#' Transform Count Data
+#'
+#' This function takes a matrix of count data and applies a transformation
+#' function (e.g., variance stabilizing transformation)
+#' to it. The transformation is executed with error handling in case the
+#' transformation fails.
+#'
+#' @param ys A matrix or data frame containing the count data to be transformed.
+#' @param fun A function to be applied to the count data. This function should
+#' accept the count data as its first argument
+#'   and optionally additional parameters like `num_cores` for parallel execution.
+#' @param num_cores number of PC cores
+#'
+#' @return The transformed count data as returned by the function \code{fun}.
+#' If the transformation fails, the function
+#'   returns \code{NULL} and an error message is printed.
+#' @export
+#'
+#' @examples
+#' ys <- matrix(rpois(20, lambda = 10), ncol = 5)
+#' transform_counts(ys, vst_transform)
+transform_counts <- function(ys, fun, num_cores) {
+  print("Transforming Counts")
+  vst_ys <- tryCatch({
+    fun(ys, num_cores = num_cores)  # Adjust num_cores as needed
+  }, error = function(e) {
+    message("Error in Counts transformation: ", e)
+    return(NULL)
+  })
+  return(vst_ys)
+}
+
+#' @export
+#' @method plot vpcest
+plot.vpcest <- function(x, ...) {
+  result_names <- c("negative_binomial", "tweedie", "gaussian")
+
+  result_order <- intersect(names(x), result_names)
+
+  true_result <- x[["true"]]
+  true_vpc0 <- true_result[,"vpc0"]
+  true_vpc1 <- true_result[,"vpc1"]
+
+  graphics::par(mfrow = c(3, 2), oma = c(0, 0, 3, 0))
+
+  for (result_name in result_order) {
+    result <- x[[result_name]]
+
+    result_vpc0 <- result[,"vpc0"]
+    result_vpc1 <- result[,"vpc1"]
+
+    plot(true_vpc0, result_vpc0 - true_vpc0, ylim = c(-1, 1), xlim=c(0,1),
+         pch = 16, col = "blue", ylab = "Bias", xlab = "True Vpc0",
+         main = result_name)
+    plot(true_vpc1, result_vpc1 - true_vpc1, ylim = c(-1, 1),  xlim=c(0,1),
+         pch = 16, col = "red", ylab = "Bias", xlab = "True Vpc1",
+         main = result_name)
+  }
+
+  graphics::mtext(paste("Data Generation Family:", result_order[1]),
+                  side = 3, line = -1, outer = TRUE, cex = 1)
+
+  graphics::par(mfrow = c(1, 1))
+}
+
+
+
+
 
